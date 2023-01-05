b5536661ab4ce355a264b9bb700eb06f87a960df
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-01-05 16:27:13 +0100

    Secure api endpoints

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,10 +6,9 @@ export const handle = (async ({ event, resolve }) => {
 
 	if (event.url.pathname.startsWith('/api')) {
 		const token = event.request.headers.get('token');
-		if (token === ENDPOINT_TOKEN) {
-			console.log('Request is allowed');
-		} else {
-			console.log('Request is NOT allowed');
+		if (token !== ENDPOINT_TOKEN) {
+			console.log('Request NOT allowed');
+			return new Response('Forbidden', { status: 403, statusText: 'Forbidden' });
 		}
 	}
 
