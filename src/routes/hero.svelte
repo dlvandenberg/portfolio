0b6a507134f9dc0154df7839240548d9731e9261
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-01-24 14:42:35 +0100

    Applied BEM to hero component Changes the fadeIn animation

--- src/routes/hero.svelte
+++ src/routes/hero.svelte
@@ -12,11 +12,11 @@
 
 <div class="hero">
 	{#if mounted}
-		<div class="hero-avatar animate" style="background-image: url('{portraitUrl}')" />
+		<div class="hero__avatar -animate" style="background-image: url('{portraitUrl}')" />
 	{/if}
-	<div class="hero-titles">
-		<h1 class="title animate">Dennis van den Berg</h1>
-		<h3 class="subtitle animate">Full Stack Developer</h3>
+	<div class="hero__titles">
+		<h1 class="hero__title -animate">Dennis van den Berg</h1>
+		<h3 class="hero__subtitle -animate">Full Stack Developer</h3>
 	</div>
 </div>
 
@@ -28,9 +28,47 @@
 		height: calc(100vh - $header-height);
 		justify-content: center;
 		margin-bottom: 5rem;
+
+		&__avatar {
+			background-color: $color-coffee;
+			background-position: 50% 50%;
+			background-size: 80% 80%;
+			background-repeat: no-repeat;
+			border: 1px solid $color-sand-100;
+			width: 15rem;
+			height: 15rem;
+			border-radius: 50% 50%;
+			margin: 2rem;
+			transition: $transition-duration ease-in-out;
+
+			&:hover {
+				transform: scale(1.3);
+				border-color: $color-yellow;
+			}
+		}
+
+		&__titles {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			align-items: center;
+			height: 6rem;
+		}
+
+		&__title {
+			font-size: x-large;
+			color: $color-sand-100;
+			font-weight: 100;
+		}
+
+		&__subtitle {
+			font-size: large;
+			font-weight: 100;
+			color: $color-sand-500;
+		}
 	}
 
-	.animate {
+	.-animate {
 		animation-duration: 0.5s;
 		animation-name: animate-fade-in;
 		animation-delay: 0.2s;
@@ -38,49 +76,14 @@
 	}
 
 	@keyframes animate-fade-in {
-		0% {
+		from {
+			transform: translate3d(0, 40px, 0);
 			opacity: 0;
 		}
-		100% {
+
+		to {
+			transform: translate3d(0, 0, 0);
 			opacity: 1;
 		}
-	}
-
-	.hero-avatar {
-		background-color: $color-coffee;
-		background-position: 50% 50%;
-		background-size: 80% 80%;
-		background-repeat: no-repeat;
-		border: 1px solid $color-sand-100;
-		width: 15rem;
-		height: 15rem;
-		border-radius: 50% 50%;
-		margin: 2rem;
-		transition: $transition-duration ease-in-out;
-
-		&:hover {
-			transform: scale(1.3);
-			border-color: $color-yellow;
-		}
-	}
-
-	.hero-titles {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-		align-items: center;
-		height: 6rem;
-	}
-
-	.title {
-		font-size: x-large;
-		color: $color-sand-100;
-		font-weight: 100;
-	}
-
-	.subtitle {
-		font-size: large;
-		font-weight: 100;
-		color: $color-sand-500;
 	}
 </style>
