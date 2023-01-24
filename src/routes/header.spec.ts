bc15108b58b4e2bc8cf47aaca80def72eadbe1cf
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-01-24 15:01:58 +0100

    Applied BEM to header and improve tests

--- src/routes/header.spec.ts
+++ src/routes/header.spec.ts
@@ -1,14 +1,27 @@
 import { render } from '@testing-library/svelte';
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import Header from './header.svelte';
 
 describe('Header.svelte', () => {
+	const selectors = {
+		brand: '.navbar__brand',
+		link: '.navbar__item-link',
+	};
+
+	const modifiers = {
+		active: '-active',
+	};
+
 	it('should render navigation bar when mounted', () => {
-		window.scrollTo = vi.fn().mockImplementation(() => {
-			return;
-		});
 		const { container } = render(Header);
-		expect(container.querySelector('.brand')).toBeInTheDocument();
-		expect(container.querySelector('.nav-menu-item a[href="/"]')).toBeInTheDocument();
+		expect(container.querySelector(selectors.brand)).toBeInTheDocument();
+		expect(container.querySelector(selectors.link)).toBeInTheDocument();
+		expect(container.querySelector(selectors.link)).toHaveAttribute('href', '/');
+	});
+
+	it('should show first link as active link', () => {
+		const { container } = render(Header);
+		const [firstLink] = container.querySelectorAll(selectors.link);
+		expect(firstLink).toHaveClass(modifiers.active);
 	});
 });
